b79f5cfe2d21c19daba7727631fec2de0724190e
Delight <86234641+delight3@users.noreply.github.com>
2023-10-17 11:15:09 +0100

    Update __init__.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -17,8 +17,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'dlghtsabina@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dsevjxjhlqqdtjyv'
+app.config['MAIL_USERNAME'] = 'MAIL USERNAME'
+app.config['MAIL_PASSWORD'] = 'MAIL PASSWORD'
 mail = Mail(app)
 
 
